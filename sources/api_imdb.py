a90bf781c91f89de4c7d30464d3de2d44d9dc3fc
Jakub Pilch <kuba.pilch2001@gmail.com>
2019-02-06 16:10:04 +0100

    Downloading number of votes. Loading and saving data to files.

--- sources/api_imdb.py
+++ sources/api_imdb.py
@@ -7,12 +7,12 @@ def progress_bar(total, current, description, prefix):
     """
     lenght = 100
     
-    percent = (float(current) * 100/float(total))
+    percent = round(float(current) * 100/float(total), 2)
     filled = int(lenght * current // total)
     bar = filled * "#" + "-" * (lenght-filled)
 
-    print("\r{0}: |{1}| {2}% [{3}] ".format(prefix, bar, percent, description))
-    
+    print("\r{0}: |{1}| {2}% [{3}]".format(prefix, bar, percent, description), end="\r")
+
     if total == current:
         print("\r")
 
@@ -40,7 +40,7 @@ class API_IMDb():
             for episode_number, episodeObject in zip(episodes.keys(), episodes.values()):
                 
                 current_episode += 1
-                progress_bar(show['number of episodes'], current_episode, "Now downloading Season {0} Episode {1}".format(season, episode_number), "Complete")
+                progress_bar(show['number of episodes'], current_episode, "Now downloading Season {0} Episode {1}".format(season, episode_number), "Downloading reviews")
 
                 # Get episode ID
                 episde_id = episodeObject.getID()
@@ -67,17 +67,51 @@ class API_IMDb():
         """
         return self.imdb.get_movie(episode_id, info=['vote details'])['arithmetic mean']
 
-    def download_number_of_votes(self):
+    def download_number_of_votes(self, save):
         """
         Downloads number of votes for a show and returns a dictionary -> {Season1:[]}
         """
-        pass
+        # Get the entire show seasons and episodes, retrives dictionary of dictionaries
+        show = self.imdb.get_movie(self.show_id, info=['episodes', 'main']) 
+        serie = show['episodes']
+
+        votes = {}
+        current_episode = 0
+
+        # Iterate through every season
+        for season, episodes in zip(serie.keys(), serie.values()):
+            
+            # Iterate through every episode in a season
+            for episode_number, episodeObject in zip(episodes.keys(), episodes.values()):
+                
+                current_episode += 1
+                progress_bar(show['number of episodes'], current_episode, "Now downloading Season {0} Episode {1}".format(season, episode_number), "Downloading votes")
+
+                # Get episode ID
+                episde_id = episodeObject.getID()
+                
+                # Download votes for episode
+                episode_review = self._download_review_for_episode(episde_id)
+
+                # Make sure that the list for this season is created and append that list with episode votes
+                if "Season{0}".format(season) in votes.keys():
+                    votes["Season{0}".format(season)].append(episode_review)
+                else:
+                    votes["Season{0}".format(season)] = [episode_review]
+        
+        # Check if user wants to save data
+        if save:
+            # Save votes to a file
+            files.save_data_to_file(votes, "votes", show['title'].replace(" ", "_"))
+        
+        return votes
+
 
     def _download_number_of_votes_for_episode(self, episode_id):
         """
         Downloads number of votes for one episode and returns it
         """
-        pass
+        return self.imdb.get_movie(episode_id)['votes']
     
     @property
     def is_show(self):
--- sources/files.py
+++ sources/files.py
@@ -1,11 +1,24 @@
 import json
 
-
 def save_data_to_file(data, data_type, show_name):
     """
     Save given data to a file with right file name
     """
-
     with open("{0}_{1}".format(data_type, show_name), "w") as f:
         json.dump(data, f)
-                    
+
+def load_data_from_file(data_type, show_name):
+    """
+    Load data from given data type and show name, check if file ex
+    """
+    try:
+        with open("{0}_{1}".format(data_type, show_name), "r") as f:
+            # File exists, load and return data
+            data = json.load(f)
+            return data
+
+    except FileNotFoundError:
+        # File does not exist, ask user if he/she wants to download it
+        raise Exception("File '{0}_{1}' does not exist, run the script with -l or -i flag to download the data first".format(data_type, show_name))
+
+load_data_from_file("votes", "inny cos")
